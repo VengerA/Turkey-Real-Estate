be983c59daa8fbdacb4ac26969acfd21df751776
VengerA <uyagmur123@gmail.com>
2019-09-14 06:44:34 +0300

    second

--- src/components/mainSearch.js
+++ src/components/mainSearch.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import location_logo from './../static/location.svg';
 import search_icon from './../static/search_icon.svg';
-import home_icon from "./../static/Home2.svg";
+import home_icon from "./../static/cerceveEv_logo.png";
 import check_icon from "./../static/check-circle.svg";
 import checked_icon from "./../static/checked-circle.svg";
+import triangle_icon from './../static/triangle.svg';
+import wallet_icon from './../static/wallet2_logo.svg';
+import bed_logo from './../static/bed_logo.svg';
+import klavye_logo from './../static/klavye_logo.svg';
 import './../App.css';
+
+import MainStore from './store.js';
+import {observer} from 'mobx-react';
+
+import cover1 from '../static/antalya.jpg'
+import cover2 from '../static/bursa.jpg'
+import cover3 from '../static/istanbul.jpg'
+import cover4 from '../static/izmi4.jpg'
+import cover5 from '../static/ankara.jpg'
+
+const coverImages = [
+    cover1,
+    cover2,
+    cover3,
+    cover4,
+    cover5,
+]
 
 class MainSection extends React.Component{
   constructor(props){
@@ -19,8 +40,44 @@ class MainSection extends React.Component{
                 {item: "Residance", selected: false},
                 {item: "Land", selected: false}, 
                 {item: "All Property Types", selected: true} 
-            ]
+            ],
+            currentImageIndex: 0,
+            imageSwitchTimer: undefined
         }
+    }
+    componentDidMount() {
+        var vm = this
+        console.log(MainStore.showProperty)
+        this.setState({ imageSwitchTimer: setInterval(() => {
+            vm.switchNextImage()
+        }, 2000) })
+    }
+
+    switchNextImage = () => {
+        let currentImageIndex = (this.state.currentImageIndex + 1) % coverImages.length
+        this.setState({ currentImageIndex })
+    }
+
+    switchToImageByIndex = (indx) => {
+        console.log('switch called with', indx)
+
+        if (indx < 0 || indx >= coverImages.length) {
+            console.log("Invalid image index:", indx, ", ( coverImages.length =", coverImages.length, ")")
+            return
+        }
+
+        var vm = this
+
+        if (this.state.imageSwitchTimer !== undefined) {
+            clearInterval(this.state.imageSwitchTimer)
+        }
+
+        this.setState({
+            currentImageIndex: indx,
+            imageSwitchTimer: setInterval(() => {
+                vm.switchNextImage()
+            }, 2000)
+        })
     }
 
     handlePropertyType = (item) => {
@@ -177,6 +234,12 @@ class MainSection extends React.Component{
                     <p className = "cityContentHeader2">Antalya</p>
                 </div>
                 <p className = "cityContentText">Antalya's warm climate, beautiful beaches and luxury resorts make it the perfect place to spend a hard-earned break, which is why it’s the first place to spring to mind whenever Turks hear the words "summer holiday." With recent investments in golf courses bringing record numbers of visitors to the area, Antalya has a whole host of things to offer, making it an unbeatable holiday destination.</p>
+                <div className = "CityContentProperities">
+                    <p className = "CityContentProperitiesText"><span className = "CityContentProperitiesText1" >234</span> PROPERTIES</p>
+                    <div>
+                        <img className = "CityContentProperitiesIcon" src ={triangle_icon}/>
+                    </div>
+                </div>
                 <div>
                     <div className = "searchContainer">
                         <p className = "Container1Text">Property Turkey For Sale</p>
@@ -187,16 +250,19 @@ class MainSection extends React.Component{
                     </div>
                     <div className = "searchOptions2">
                         <div className = "searchOptions" onClick = {this.showPropertyType}>
-                            <icon src = {home_icon} className = "MainSearchIcons" />
+                            <img src = {home_icon} className = "MainSearchIcons" />
                             <p className ="searchOptionText">3 Property Types</p>
                         </div>
                         <div className = "searchOptions" onClick = {() => this.showPrice()}>
+                            <img src = {wallet_icon} className = "MainSearchIcons" />
                             <p className ="searchOptionText">Any Price</p>
                         </div>
                         <div className = "searchOptions">
+                            <img src = {bed_logo} className = "MainSearchIcons" />
                             <p className ="searchOptionText">All Bed Rooms</p>
                         </div>
                         <div className = "searchOptions" onClick = {() =>this.showAdvanced()}>
+                            <img src = {klavye_logo} className = "MainSearchIcons" />
                             <p className ="searchOptionText">Advanced Filter</p>
                         </div>
                     </div>
@@ -205,11 +271,57 @@ class MainSection extends React.Component{
                         <img className = "searchIcon" src = {search_icon}/>
                     </div>
                 </div>
+                <div className="SliderGallery">
+                    <img
+                        className="slider-cover-image"
+                        src={coverImages[this.state.currentImageIndex]}
+                    />
+                    <div className="slider-imageslider" style={styles.slider}>
+                        {coverImages.map((uri, index) => (
+                            <div
+                                style={this.state.currentImageIndex == index ? styles.sliderItemSelected : styles.sliderItem}
+                                onClick={() => {this.switchToImageByIndex(index)}}
+                                className="slider-imagesilder-item"
+                            >
+                                {index + 1}
+                            </div>
+                        ))}
+                    </div>
+                </div>
                 {PropertyTypesHover()}
                 {Price()}
                 {AdvancedFilter()}
             </div>
         );
+    }
+}
+
+const styles = {
+    slider: {
+        position: 'absolute',
+        top: '209px',
+        right: '64px',
+        width: '139px',
+        textAlign: 'center',
+        height: '191px',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        overflowY: 'scroll',
+        scrollbarColor: 'white rgba(255, 255, 255, 0.4)',
+        scrollbarWidth: 'thin'
+    },
+    sliderItem: {
+        fontSize: '18.6px',
+        color: '#fff',
+        fontWeight: 'bold',
+        marginBottom: '15px',
+        marginTop: '15px'
+    },
+    sliderItemSelected: {
+        fontSize: '41px',
+        color: '#fff',
+        fontWeight: 'bold'
     }
 }
 
---
+++ src/components/store.js
@@ -0,0 +1,10 @@
+import { observable, action } from "mobx";
+
+class MainStore {
+    showProperty = false
+
+    
+}
+
+
+export default MainStore;
