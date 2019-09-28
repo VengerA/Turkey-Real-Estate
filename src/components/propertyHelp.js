9fa551445b29fc7623cbb7f09edfab82f7965115
ozansz <ozan.sazak@protonmail.ch>
2019-09-28 05:49:40 +0300

    mailing api comm

--- src/components/propertyHelp.js
+++ src/components/propertyHelp.js
@@ -4,18 +4,65 @@ import whatsapp_icon from './../static/whatsapp_logo.svg';
 import helper_girl from './../static/helperGirl.svg'
 import './../App.css';
 
+import axios from 'axios';
+
 class PropertyHelp extends React.Component{
   constructor(props){
     super(props);
     this.state = {  
+        clientName: '',
+        clientPhone: '',
+        clientEmail: '',
+        clientMessage: '',
     }
   }
+
+  onNameChange = (e) => {
+    this.setState({ clientName: e.target.value })
+  }
+
+  onPhoneChange = (e) => {
+    this.setState({ clientPhone: e.target.value })
+  }
+
+  onEmailChange = (e) => {
+    this.setState({ clientEmail: e.target.value })
+  }
+
+  onMessageChange = (e) => {
+    this.setState({ clientMessage: e.target.value })
+  }
+
+  onSubmitHandler = (e) => {
+      e.preventDefault()
+
+      if (this.state.clientName == '' || this.state.clientPhone == '' || this.props.property === undefined) {
+          return
+      }
+
+      axios.post('http://138.201.16.232/properties/send-mail/', {
+          original_url: 'https://www.hurriyetemlak.com/antalya-kemer-merkez-satilik/villa/'+this.props.property.listing_id,
+          url: 'http://turkeyrealestate.com/property?pid='+this.props.property.id,
+          id: this.props.property.id,
+          customer_name: this.state.clientName,
+          customer_phone: this.state.clientPhone,
+          customer_email: this.state.clientEmail,
+          customer_message: this.state.clientMessage,
+          owner_name: this.props.property.publisher,
+          owner_phone: JSON.parse(this.props.property.phone_numbers)[0]
+      })
+        .then(res => {
+            alert("Your response was saved succesfully.")
+            window.location = '/'
+        })
+  }
+
   render(){
     
     return (
       <aside id="sidebar" class="col-lg-3 col-12">
           <div class="sidebar-block">
-              <form action="#" class="sidebar-form">
+              <form class="sidebar-form" onSubmit={this.onSubmitHandler}>
                   <div class="form-title">
                       How we can help you?
                   </div>
@@ -27,23 +74,23 @@ class PropertyHelp extends React.Component{
                   <div class="row mb-3">
                       <div class="col-lg-6 col-12">
                           <div class="form-group">
-                              <input type="text" placeholder="Full Name"/>
+                              <input type="text" placeholder="Full Name" value={this.state.clientName} onChange={this.onNameChange}/>
                           </div>
                       </div>
 
                       <div class="col-lg-6 col-12">
                           <div class="form-group">
-                              <input type="text" placeholder="Email"/>
+                              <input type="text" placeholder="Email" value={this.state.clientEmail} onChange={this.onEmailChange}/>
                           </div>
                       </div>
                   </div>
 
                   <div class="form-group">
-                      <input type="text" placeholder="Phone Number"/>
+                      <input type="text" placeholder="Phone Number" value={this.state.clientPhone} onChange={this.onPhoneChange} />
                   </div>
 
                   <div class="form-group">
-                      <input type="text" placeholder="Message"/>
+                      <input type="text" placeholder="Message" value={this.state.clientMessage} onChange={this.onMessageChange}/>
                   </div>
 
                   <div class="form-group">
